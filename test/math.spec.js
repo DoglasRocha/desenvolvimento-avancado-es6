5733f44facffd81ec3d9425f11ae72fad752de79
DoglasRocha <doglas_rocha@protonmail.ch>
2021-09-05 19:36:43 -0300

    work with sinon

--- test/math.spec.js
+++ test/math.spec.js
@@ -1,13 +1,15 @@
-const assert = require('assert');
 const Math = require('../src/math.js');
 // importing chai
 const expect = require('chai').expect;
+// importing sinon
+const sinon = require('sinon');
 
 let value = 0;
 
 // descrição do comportamento
 describe('Math class', () => {
 
+    // hooks
     beforeEach(function() {
     // instruções a serem executadas antes de cada teste
         value = 0;
@@ -20,10 +22,11 @@ describe('Math class', () => {
 
     // comportamento esperado
     it('Sum two numbers', function(done) {
+        const math = new Math();
+
         value = 6;
         // seta um novo valor para o tempo de teste, que por padrão é de 2000ms
         this.timeout(3000);
-        const math = new Math();
         // verifica se os valores são iguais
         math.sum(value, 5, value => {
             /*
@@ -37,8 +40,10 @@ describe('Math class', () => {
     });
     // quando se coloca only, ele executa apenas o teste com only
     // it.only('Multiply two numbers', function() {
+
     it('Multiply two numbers', function() {
         const math = new Math();
+
         //           func                valor esperado
         expect(math.multiply(value, 5)).to.equal(0);
     });
@@ -46,6 +51,8 @@ describe('Math class', () => {
     it.skip('Divide two numbers', function() {})
 
     it('Verifying if object has property', function() {
+        const math = new Math();
+
         const obj = {name: 'Doglas'};
 
         // verifying if object has property name AND verifying the value of the property
@@ -53,10 +60,33 @@ describe('Math class', () => {
     });
 
     it('Verifying if two objects are equal', function() {
+        const math = new Math();
+        
         const obj = {name: 'Doglas'};
         const obj2 = {name: 'Doglas'};
 
         expect(obj).to.deep.equal(obj2);
+    });
+
+    it('Calls res with sum and index values', function() {
+        const math = new Math();
+        
+        const req = {};
+        const res = {
+            load: sinon.spy()
+        }
+
+        // adding to already created method
+        /* sinon.spy(object, 'method') */
+        // adding a customized return to the spy
+        /* sinon.stub(obj, 'method').returns('thing to return') */
+        // returning the method to its original implementation
+        /* obj.restore() */
+
+        math.printSum(req, res, 5, 5);
+
+        expect(res.load.calledOnce).to.be.true;
+        // verifying if the response was loaded in the method
     })
 });
 
